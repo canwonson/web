fd6f85bd6f678e4e147053cdd48b73b262af773d
canwonson <133451314@qq.com>
2016-02-02 18:02:06 +0800

    changes log

--- Application/DataGrabber/Controller/ImgController.class.php
+++ Application/DataGrabber/Controller/ImgController.class.php
@@ -5,36 +5,33 @@ use QL\QueryList;
 
 class ImgController extends Controller
 {
-	/*public function index(){
-		$num = I('post.num',10);
+	public function index(){
+		$num = I('post.num',5);
 		$p = I('post.p',1);
 		$blog = I('post.blog');
-		$size = I('post.size');
+		$size = I('post.size',5);
+		$big_size = I('post.big_size',3);
 		$start = ($p - 1)*$num;
 		$url = 'http://'.$blog.'.tumblr.com/api/read?type=photo&num='.$num.'&start='.$start;
-		dump($url);
 		$rules = array(
-			'img_1280'  => array('post>photoset>photo>photo-url[max-width="1280"]','text'),
-			'img_500'  => array('post>photoset>photo>photo-url[max-width="500"]','text'),
-			'img_250'  => array('post>photoset>photo>photo-url[max-width="250"]','text'),
-			'img_100'  => array('post>photoset>photo>photo-url[max-width="100"]','text')
+			'0'  => array('post>photoset>photo>photo-url[max-width="1280"]','text'),
+			'1'  => array('post>photoset>photo>photo-url[max-width="500"]','text'),
+			'2'  => array('post>photoset>photo>photo-url[max-width="400"]','text'),
+			'3'  => array('post>photoset>photo>photo-url[max-width="250"]','text'),
+			'4'  => array('post>photoset>photo>photo-url[max-width="100"]','text'),
+			'5'  => array('post>photoset>photo>photo-url[max-width="75"]','text')
 			);
 		$data = QueryList::Query($url,$rules)->data;
-		foreach ($data as $key => $value) {
-			$img_size = 'img_'.$size;
-			$list[] = $value[$img_size];
-		}
-		$img_size = 'img_'.$size;
-		$this->assign('list',$list);
+		$this->assign('list',$data);
 		$this->assign('blog',$blog);
 		$this->assign('size',$size);
-		$p = $p+1;
+		$this->assign('big_size',$big_size);
 		$this->assign('p',$p);
 		$this->assign('num',$num);
 		$this->display();
-	}*/
+	}
 
-	public function index(){
+	/*public function index(){
 		$num = I('post.num',20);
 		$p = I('post.p',1);
 		$blog = I('post.blog');
@@ -68,7 +65,7 @@ class ImgController extends Controller
 		$this->assign('p',$p);
 		$this->assign('num',$num);
 		$this->display();
-	}
+	}*/
 }
 
 ?>
