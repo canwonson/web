322ce35364656dab98d9568f3288107e6d9e2943
canwonson <133451314@qq.com>
2016-02-02 12:51:41 +0800

    changes log

---
+++ Application/DataGrabber/Controller/ImgController.class.php
@@ -0,0 +1,31 @@
+<?php
+namespace DataGrabber\Controller;
+use Think\Controller;
+use QL\QueryList;
+
+class ImgController extends Controller
+{
+	public function index(){
+		$num = 10;
+		$p = I('get.p',1);
+		$start = ($p - 1)*$num;
+		$blog = I('get.b');
+		$size = I('get.s');
+		$url = 'http://'.$blog.'.tumblr.com/api/read?type=photo&num='.$num.'&start='.$start;
+		dump($url);
+		$rules = array(
+			'img_1280'  => array('post>photoset>photo>photo-url[max-width="1280"]','text'),
+			'img_500'  => array('post>photoset>photo>photo-url[max-width="500"]','text'),
+			'img_250'  => array('post>photoset>photo>photo-url[max-width="250"]','text'),
+			'img_100'  => array('post>photoset>photo>photo-url[max-width="100"]','text')
+			);
+		$data = QueryList::Query($url,$rules)->data;
+		foreach ($data as $key => $value) {
+			$img_size = 'img_'.$size;
+			echo '<img src="'.$value[$img_size].'">';
+			echo '<br/>';
+		}
+	}
+}
+
+?>
